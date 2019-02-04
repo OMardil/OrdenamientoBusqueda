8ea621199d8a3e4b42e33ae70ae0737340e6441c
Omar <Omar@10.232.178.161>
2019-02-04 13:03:48 -0600

    ç

--- StaticMethodsDemo/src/staticmethodsdemo/StaticMethodsDemo.java
+++ StaticMethodsDemo/src/staticmethodsdemo/StaticMethodsDemo.java
@@ -45,7 +45,9 @@ public class StaticMethodsDemo {
         return (num1 + num2) / 2.0;
     }
     
-    public static double calculateAverage(double num1, double num2, double num3) {
+    public static double calculateAverage(double num1, 
+                                          double num2, 
+                                          double num3) {
         return (num1 + num2 + num3) / 3.0;
     }
        
