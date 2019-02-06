0afff54896cf72f84cc67e5d3c709991a245cc3a
Omar <Omar@192.168.1.71>
2019-02-05 21:28:46 -0600

    Finalized development on both classes, testing is still required.

--- OrdenamientoBusqueda/src/ordenamientobusqueda/OrdenamientoBusqueda.java
+++ OrdenamientoBusqueda/src/ordenamientobusqueda/OrdenamientoBusqueda.java
@@ -4,6 +4,28 @@ package ordenamientobusqueda;
  * @author Omar
  */
 public class OrdenamientoBusqueda {
+    
+    //implementation of Selection sort
+    public void selectionSort(int[] array) {
+        
+        //Ony by one move boundary of unsorted array
+        for(int i = 0; i < array.length-1; i++) {
+            
+            //Find the minimum element in unsorted array
+            int min_idx = i;
+            for(int j = i+1; j<array.length; j++){
+                if (array[j] < array[min_idx]) {
+                    min_idx = j;
+                }
+            }
+            
+            //swap the minimum found element with the first unsorted element
+            int temp = array[min_idx];
+            array[min_idx] = array[i];
+            array[i] = temp;
+        
+        }
+    }
 
     public static int findElement(int[] array, int key) {
 
